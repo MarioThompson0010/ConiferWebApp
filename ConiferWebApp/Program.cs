512d4c1459aa44ca58f361e7264ec08021c80667
mariothompson0010@gmail.com <mariothompson0010@gmail.com>
2024-04-25 11:35:00 -0500

    Completed assignment

--- ConiferWebApp/Program.cs
+++ ConiferWebApp/Program.cs
@@ -49,6 +49,25 @@ builder.Services.AddHttpClient<IState, State>("myapist", c =>
 });
 
 
+//using System.Net.Http.Headers;
+//var client = new HttpClient();
+//var request = new HttpRequestMessage
+//{
+//    Method = HttpMethod.Get,
+//    RequestUri = new Uri("https://national-weather-service.p.rapidapi.com/zones/%7Btype%7D/%7BzoneId%7D/forecast"),
+//    Headers =
+//    {
+//        { "X-RapidAPI-Key", "a2b5d3b684mshabbf5412b1d3507p11b0a1jsnd2cd92016a40" },
+//        { "X-RapidAPI-Host", "national-weather-service.p.rapidapi.com" },
+//    },
+//};
+//using (var response = await client.SendAsync(request))
+//{
+//    response.EnsureSuccessStatusCode();
+//    var body = await response.Content.ReadAsStringAsync();
+//    Console.WriteLine(body);
+//}
+
 
 /////////////////////////////////////////////////////   /
 
